560e008e366855365e1a7d4cc21c56716cd7057f
cepi-ramdhani <cepiramdhani13@gmail.com>
2025-12-14 18:11:17 +0700

    finalize the app

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -2,9 +2,9 @@ import { notFound } from "next/navigation";
 import Image from "next/image";
 import BookEvent from "@/components/BookEvent";
 import { IEvent } from "@/database";
-import  EventCard  from "@/components/EventCard";
+import EventCard from "@/components/EventCard";
 import { getSimilarEventsBySlug } from "@/lib/actions/event.actions";
-
+import { cacheLife } from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -28,8 +28,8 @@ const EventAgenda = ({ agendaItems = [] }: { agendaItems?: string[] }) => (
     <h2>Agenda</h2>
     <ul>
       {agendaItems.map((item) => {
-        const [timeRaw, titleRaw] = item.split('|').map((s) => s.trim());
-        const time = timeRaw ?? '';
+        const [timeRaw, titleRaw] = item.split("|").map((s) => s.trim());
+        const time = timeRaw ?? "";
         const title = titleRaw ?? item;
         return (
           <li key={item} className="agenda-item">
@@ -45,40 +45,48 @@ const EventAgenda = ({ agendaItems = [] }: { agendaItems?: string[] }) => (
 const EventTags = ({ tags = [] }: { tags?: string[] }) => (
   <div className="flex flex-row gap-2 flex-wrap">
     {tags.map((tag) => (
-      <div className="pill" key={tag} >{tag}</div>
+      <div className="pill" key={tag}>
+        {tag}
+      </div>
     ))}
   </div>
-)
+);
 
-const EventDetailPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
+const EventDetailPage = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  "use cache";
+  cacheLife("hours");
   const { slug } = await params;
+
   const request = await fetch(`${BASE_URL}/api/events/${slug}`);
+  const { event } = await request.json();
+
   const {
-    event: {
-      title,
-      description,
-      image,
-      overview,
-      date,
-      time,
-      venue,
-      location,
-      mode,
-      agenda,
-      audience,
-      tags,
-      organizer,
-    },
-  } = await request.json();
+    title,
+    description,
+    image,
+    overview,
+    date,
+    time,
+    venue,
+    location,
+    mode,
+    agenda,
+    audience,
+    tags,
+    organizer,
+  } = event;
 
   if (!description) return notFound();
 
-  const bookings = 10
+  const bookings = 10;
 
   const similarEvents: IEvent[] = await getSimilarEventsBySlug(slug);
 
   //console.log({similarEvents})
-
 
   return (
     <section id="event">
@@ -146,7 +154,6 @@ const EventDetailPage = async ({ params }: { params: Promise<{ slug: string }> }
           </section>
 
           <EventTags tags={tags} />
-
         </div>
 
         {/* Right Side - Booking Form */}
@@ -154,12 +161,13 @@ const EventDetailPage = async ({ params }: { params: Promise<{ slug: string }> }
           <div className="signup-card">
             <h2>Book Your Spot</h2>
             {bookings > 0 ? (
-          <p className="text-sm">
-            Join {bookings} people who have already booked their spot!
-          </p>  ):(
-            <p className="text-sm">Be the first to book your spot! </p>
-          ) }
-          <BookEvent />
+              <p className="text-sm">
+                Join {bookings} people who have already booked their spot!
+              </p>
+            ) : (
+              <p className="text-sm">Be the first to book your spot! </p>
+            )}
+            <BookEvent eventId={event._id} slug={event.slug} />
           </div>
         </aside>
       </div>
@@ -167,11 +175,16 @@ const EventDetailPage = async ({ params }: { params: Promise<{ slug: string }> }
       <div className="flex w-full flex-col gap-4 pt-20">
         <h2>Similar Events</h2>
         <div className="events">
-          {similarEvents.length > 0 && similarEvents.map((similarEvent: IEvent) => (
-            <EventCard key={similarEvent.slug} {...similarEvent} imageWidth={800} imageHeight={800} />
-          ))}
+          {similarEvents.length > 0 &&
+            similarEvents.map((similarEvent: IEvent) => (
+              <EventCard
+                key={similarEvent.slug}
+                {...similarEvent}
+                imageWidth={800}
+                imageHeight={800}
+              />
+            ))}
         </div>
-
       </div>
     </section>
   );
